eca6daf9dde4dbff7b33f96d99518a20f08e8e61
katschre <schreiber.katrina@gmail.com>
2016-04-21 02:22:39 +0000

    hooking up next lesson button

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,13 @@ class Lesson < ActiveRecord::Base
   include RankedModel
   ranks :row_order, :with_same => :section_id
   
+
+  def next_lesson
+    #lookup the next lesson within its section. if there is none, it will produce the value nil. 
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+
+    return lesson
+  end
+
+
 end
